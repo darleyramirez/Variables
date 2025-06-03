1137ab12fd14cc67f1d8fd1f0cebb7234c8051ee
Anselmo Ortega <lfkanselmo@gmail.com>
2025-06-03 11:05:25 -0500

    variables

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,61 @@
+package org.example;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("variables");
+        int edad;
+        double precioProducto;
+        int cantidadHijos;
+        double salario;
+        double altura;
+        boolean interruptorEncendido;
+        char genero;
+        String matriculaVehiculo;
+        int productosDisponibles;
+        double distanciaKm;
+        double temperatura;
+        String numeroTelefono;
+        LocalDate fechaNacimiento;
+        String codigoPostal;
+        int idProducto;
+        int puntajeExamen;
+        double velocidadAuto;
+        boolean esMayorDeEdad;
+        boolean puertaAbierta;
+        long poblacionMundial;
+        char letraExamen;
+        String palabra;
+        int estrellasCalificacion;
+        boolean archivoActivo;
+        double precioBilleteAvion;
+        int puntuacionVideojuego;
+        boolean tieneLicenciaConduccion;
+        double calificacionPromedio;
+        String nombreCompleto;
+        int cantidadLibros;
+        String codigoBarras;
+        double porcentajeBateria;
+        double tipoCambioDivisas;
+        String numeroMatriculaEstudiante;
+        double puntajePromedioDeportista;
+        char caracterEspecial;
+        double constantePlanck;
+        boolean sensorFuncionando;
+        LocalTime horaEvento;
+        boolean clienteRegistrado;
+        String idCliente;
+        double volumenRecipiente;
+        String nombreUsuario;
+        String codigoProducto;
+        int cantidadVisitantes;
+        boolean pagoConfirmado;
+        double pesoPersona;
+        double notaFinal;
+        char teclaPulsada;
+        String nombreEmpresa;
+
+    }
+}
